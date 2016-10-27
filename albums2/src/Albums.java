749d461d91a48ae26794a7db6c3626385175a5a1
Eldio <dremon@iespoblenou.org>
2016-10-27 16:08:46 +0200

    hola Radha

---
+++ albums2/src/Albums.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Dionis on 22/11/2015.
+ */
+public class Albums {
+    private int id;
+    private String titol;
+    private String autor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
---
+++ albums2/src/connection.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Dionis on 22/11/2015.
+ */
+public class connection {
+
+}
